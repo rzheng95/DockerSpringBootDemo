bdd18fbc947acdf2289ec042755f04087994b274
Richard Zheng <ruixiangzheng95@gmail.com>
2022-07-10 23:20:03 -0500

    Initial commit.

---
+++ src/main/java/com/rzheng/dockerspringbootdemo/DockerSpringBootDemoApplication.java
@@ -0,0 +1,21 @@
+package com.rzheng.dockerspringbootdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DockerSpringBootDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DockerSpringBootDemoApplication.class, args);
+    }
+
+
+    @GetMapping("/hello")
+    public String helloWorld() {
+        return "Hello World! Richard Zheng!!!";
+    }
+}
---
+++ src/test/java/com/rzheng/dockerspringbootdemo/DockerSpringBootDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rzheng.dockerspringbootdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerSpringBootDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
